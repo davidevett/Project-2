12fe4a658705d5a6b85ff415bb41fe32f1ae46fc
RobertUlloa <roanuc8@gmail.com>
2024-08-22 20:00:18 -0500

    fixing the fetch request for extra info and geting images for the results

--- utils/foursquare.js
+++ utils/foursquare.js
@@ -12,11 +12,24 @@ const searchFoursquare = async (location, query) => {
       params: {
         query,
         near: location,
-        limit: 50, // limit of results
+        limit: 50, // Limit of results
       },
     });
 
-    return response.data.results;
+    const places = response.data.results;
+
+    // Fetch photos for each place
+    for (const place of places) {
+      const photoEndpoint = `https://api.foursquare.com/v3/places/${place.fsq_id}/photos`;
+      const photoResponse = await axios.get(photoEndpoint, {
+        headers: {
+          Authorization: apiKey,
+        },
+      });
+      place.photos = photoResponse.data;
+    }
+
+    return places;
   } catch (error) {
     console.error('Error fetching data from Foursquare API:', error);
     return [];
